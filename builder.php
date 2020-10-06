dcb34ff9b05d889b9f8fc4ec3ab1ae1d3553f007
amr <alexmakeresponsive@gmail.com>
2020-10-06 19:15:36 +0600

    init

---
+++ builder.php
@@ -0,0 +1,22 @@
+$sql = "SELECT * FROM comment WHERE post_id = 1;";
+
+$count = 0;
+
+$tree = [];
+
+foreach ($dbn->query($sql) as $row)
+{
+    $idCurrent = $row['id'];
+    $idParent  = $row['parent_id'];
+
+    $tree[$idParent]['child_list'][$idCurrent] = [
+        'user_id' => $row['user_id'],
+        'text'    => $row['text'],
+    ];
+
+    $tree[$idCurrent] = &$tree[$idParent]['child_list'][$idCurrent];
+}
+
+$parent = array_shift($tree);
+
+var_dump($parent['child_list']);
